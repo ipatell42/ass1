db9908ca53412db76b58278b3b63a3d9b5b17fb2
EC2 Default User <ec2-user@ip-172-31-13-101.ec2.internal>
2023-06-03 05:02:22 +0000

    first commit

---
+++ modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Ish"
+    "App"   = "Chachus"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "ass1"
+}
